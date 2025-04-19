561fa902c5dc1c4d3ba7f514e4612986548e2842
Pavankhaju <khajurkarp@gmail.com>
2025-04-19 19:21:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def webhook():
     return jsonify({"fulfillmentText": reply_text})
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=5000)
+    app.run(debug=True,host="0.0.0.0", port=10000)
