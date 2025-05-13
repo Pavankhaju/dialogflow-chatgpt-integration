46f0609e80622b8a1ccc8e80fd3ff845a62a55b8
Pavankhaju <khajurkarp@gmail.com>
2025-05-13 21:39:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import openai
 app = Flask(__name__)
 
 # OpenAI API Key
-openai.api_key= "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"
+OPENAI_API_KEY= ("OPENAI_API_KEY")
 # Health check / root route
 @app.route("/", methods=["GET"])
 def health_check():
@@ -33,4 +33,4 @@ def webhook():
     return jsonify({"fulfillmentText": reply_text})
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=8000)
+    app.run(debug=True,host="0.0.0.0", port=5000)
